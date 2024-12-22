e42a1cf8cd8bc927dc0fc6bde228ff3f88324ef0
LinggaKaindra <lingga.kaindra@gmail.com>
2024-12-22 10:59:55 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,5 +10,35 @@
     <h1>Lanjutkan sebagai?</h1>
     <p><a href="petugas/index.php">Petugas</a> || <a href="siswa/index.php">Siswa</a></p>
     
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Pilihan</title>
+</head>
+<body>
+    <div class="OptionBox">
+      <h1>Lanjutkan sebagai?</h1>
+      <div class="Opsi Petugas">
+        <form action="petugas/index.php">
+          <div class="input-area">
+            <input type="submit" value="Petugas">
+          </div>
+        </form>
+      </div>
+      <div class="Opsi Siswa">
+        <form action="siswa/index.php">
+          <div class="input-area">
+            <input type="submit" value="Siswa">
+          </div>
+        </form>
+      </div>
+    </div>
 </body>
 </html>
