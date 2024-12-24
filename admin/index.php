22e14ca0e14fc073db746db7799f6a1d75d2419f
LinggaKaindra <lingga.kaindra@gmail.com>
2024-12-25 00:53:46 +0700

    update index.php file

--- admin/index.php
+++ admin/index.php
@@ -16,7 +16,7 @@ require "../functions.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../NavbarStyle.css">
+    <link rel="stylesheet" href="../Assets/NavbarStyle.css">
     <title>Document</title>
 </head>
 <body>
