5ea50f4a662a74006f9bb849251369e030a68d9b
LinggaKaindra <lingga.kaindra@gmail.com>
2024-12-25 00:04:08 +0700

    update tambahPeng.php file

--- admin/users/tambahPeng.php
+++ admin/users/tambahPeng.php
@@ -15,31 +15,40 @@ if (isset($_POST["submit"])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <h1>Halaman Registrasi Pengguna</h1>
-    <form action="" method="post">
-        <label for="username">Username : </label>
-        <input type="text" name="username" id="username" autocomplete="off">
-        <br>
-        <label for="email">email : </label>
-        <input type="email" name="email" id="email" autocomplete="off">
-        <br>
-        <label for="password">Password : </label>
-        <input type="password" name="password" id="password" autocomplete="off">
-        <br>
-        <label for="level">Role : </label>
-        <select name="level" id="level">
-            <option value="admin">Admin</option>
-            <option value="petugas">Petugas</option>
-        </select>
-        <br>
-        <button type="submit" name="submit">Kirim</button>
-    </form>
-    
+    <link rel="stylesheet" href="../LoginPetugasSiswaStyle.css">
+    <title>Registrasi Petugas</title>
 </head>
 <body>
-    
+      <div class="wrapper">
+        <header>Halaman Registrasi Pengguna</header>
+        <form action="" method="post">
+          <div class="field username">
+            <div class="input-area">
+              <input type="text" name="username"  id="username" autocomplete="off" placeholder="Username">
+            </div>
+          </div>
+          <div class="field email">
+            <div class="input-area">
+              <input type="email" name="email" id="email" autocomplete="off" placeholder="Email">
+            </div>
+          </div>
+          <div class="field password">
+            <div class="input-area">
+              <input type="password" name="password" id="password" autocomplete="off" placeholder="Password">
+            </div>
+          </div>
+          <div class="field role">
+            <div class="select-area">
+                <select name="level" id="level">
+                    <option value="admin">Admin</option>
+                    <option value="petugas">Petugas</option>
+                </select>
+            </div>
+          </div>
+          <input type="submit" value="Kirim">
+        </form>
+      </div>
 </body>
 </html>
