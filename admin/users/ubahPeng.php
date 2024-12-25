d3d6dbb91d33f987e34da3bbf8b250ffca1a09ea
LinggaKaindra <lingga.kaindra@gmail.com>
2024-12-25 14:08:06 +0700

    update ubahpeng

--- admin/users/ubahPeng.php
+++ admin/users/ubahPeng.php
@@ -22,32 +22,41 @@ if (isset($_POST["submit"])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <h1>Halaman Registrasi Pengguna</h1>
-    <form action="" method="post">
-        <input type="hidden" name="id" value="<?= $pengguna[0]["id"]; ?>">
-        <label for="username">Username : </label>
-        <input type="text" name="username" id="username" autocomplete="off" value="<?= $pengguna[0]["username"]; ?>">
-        <br>
-        <label for="email">email : </label>
-        <input type="email" name="email" id="email" autocomplete="off" value="<?= $pengguna[0]["email"]; ?>">
-        <br>
-        <label for="password">Password : </label>
-        <input type="password" name="password" id="password" autocomplete="off" value="<?= $pengguna[0]["password"]; ?>">
-        <br>
-        <label for="level">Role : </label>
-        <select name="level" id="level">
-            <option value="admin" >Admin</option>
-            <option value="petugas">Petugas</option>
-        </select>
-        <br>
-        <button type="submit" name="submit">Kirim</button>
-    </form>
-    
+    <link rel="stylesheet" href="../../Assets/CSS/LoginPetugasSiswaStyle.css">
+    <title>Ubah data Registrasi Pengguna</title>
 </head>
 <body>
-    
+  <input type="hidden" name="id" value="<?= $pengguna[0]["id"]; ?>">
+      <div class="wrapper">
+        <header>Halaman Registrasi Pengguna</header>
+        <form action="" method="post">
+          <div class="field username">
+            <div class="input-area">
+              <input type="text" name="username" id="username" autocomplete="off" value="<?= $pengguna[0]["username"]; ?>">
+            </div>
+          </div>
+          <div class="field email">
+            <div class="input-area">
+              <input type="email" name="email" id="email" autocomplete="off" value="<?= $pengguna[0]["email"]; ?>">
+            </div>
+          </div>
+          <div class="field password">
+            <div class="input-area">
+              <input type="password" name="password" id="password" autocomplete="off" value="<?= $pengguna[0]["password"]; ?>">
+            </div>
+          </div>
+          <div class="field role">
+            <div class="select-area">
+              <select name="level" id="level">
+                <option value="admin" >Admin</option>
+                <option value="petugas">Petugas</option>
+              </select>
+            </div>
+          </div>
+          <input type="submit" name="submit" value="Kirim">
+        </form>
+      </div>
 </body>
 </html>
