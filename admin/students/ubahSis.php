3aeb519efb85d8734788b053d8c639af373e2d0b
LinggaKaindra <lingga.kaindra@gmail.com>
2024-12-25 14:07:48 +0700

    update ubahsis

--- admin/students/ubahSis.php
+++ admin/students/ubahSis.php
@@ -29,39 +29,50 @@ if (isset($_POST["submit"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../../Assets/CSS/LoginPetugasSiswaStyle.css">
+    <title>Ubah Data Siswa</title>
 </head>
 <body>
-    
-    <h1>Ubah Data Siswa</h1>
-
-    <form action="" method="post">
-        <input type="hidden" name="id" value="<?= $siswa["id"]; ?>">
-        <label for="nim">nim : </label>
-        <input type="text" name="nim" id="nim" autocomplete="off" value="<?= $siswa["nim"]; ?>">
-        <br>
-        <label for="name">name : </label>
-        <input type="text" name="name" id="name" autocomplete="off" value="<?= $siswa["name"]; ?>">
-        <br>
-        <label for="email">email : </label>
-        <input type="email" name="email" id="email" autocomplete="off" value="<?= $siswa["email"]; ?>">
-        <br>
-        <label for="password">password : </label>
-        <input type="password" name="password" id="password" autocomplete="off" value="<?= $siswa["password"]; ?>">
-        <br>
-        <label for="phone">No Telp. : </label>
-        <input type="text" name="phone" id="phone" autocomplete="off" value="<?= $siswa["phone"]; ?>">
-        <br>
-        <label for="program_id">Pilih Kelas : </label>
-        
-        <select name="program_id" id="program_id">
-            <?php foreach ($programs as $row): ?>
-                <option value="<?php echo $row['id']; ?>"><?= $row['faculty'] ?></option>
-            <?php endforeach; ?>
-        </select>
-        <br>
-        <button type="submit" name="submit">Kirim</button>
-    </form>
-    
+  <input type="hidden" name="id" value="<?= $pengguna[0]["id"]; ?>">
+      <div class="wrapper">
+        <header>Ubah Data Siswa</header>
+        <form action="" method="post">
+          <div class="field studentid">
+            <div class="input-area">
+              <input type="text" name="nim" id="nim" autocomplete="off" value="<?= $siswa["nim"]; ?>">
+            </div>
+          </div>
+          <div class="field name">
+            <div class="input-area">
+              <input type="text" name="name" id="name" autocomplete="off" value="<?= $siswa["name"]; ?>">
+            </div>
+          </div>
+          <div class="field email">
+            <div class="input-area">
+              <input type="email" name="email" id="email" autocomplete="off" value="<?= $siswa["email"]; ?>">
+            </div>
+          </div>
+          <div class="field password">
+            <div class="input-area">
+              <input type="password" name="password" id="password" autocomplete="off" value="<?= $siswa["password"]; ?>">
+            </div>
+          </div>
+          <div class="field phone">
+            <div class="input-area">
+              <input type="text" name="phone" id="phone" autocomplete="off" value="<?= $siswa["phone"]; ?>">
+            </div>
+          </div>
+          <div class="field program_id">
+            <div class="select-area">
+              <select name="program_id" id="program_id">
+                <?php foreach ($programs as $row): ?>
+                    <option value="<?php echo $row['id']; ?>"><?= $row['faculty'] ?></option>
+                <?php endforeach; ?>
+              </select>
+            </div>
+          </div>
+          <input type="submit" name="submit" value="Kirim">
+        </form>
+      </div>
 </body>
 </html>
