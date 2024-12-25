232d0b4ab93e5ce78b4438a81588ced7c4dd63c3
LinggaKaindra <lingga.kaindra@gmail.com>
2024-12-25 14:07:37 +0700

    update tambahprodi

--- admin/programs/tambahProdi.php
+++ admin/programs/tambahProdi.php
@@ -15,22 +15,27 @@ if (isset($_POST["submit"])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <h1>Halaman Tambah Prodi</h1>
-    <form action="" method="post">
-        <label for="nama">nama : </label>
-        <input type="text" name="nama" id="nama" autocomplete="off">
-        <br>
-        <label for="jurusan">jurusan : </label>
-        <input type="text" name="jurusan" id="jurusan" autocomplete="off">
-        <br>
-        <button type="submit" name="submit">Kirim</button>
-    </form>
-    
+    <link rel="stylesheet" href="../../Assets/CSS/LoginPetugasSiswaStyle.css">
+    <title>Tambah Prodi</title>
 </head>
 <body>
-    
+      <div class="wrapper">
+        <header>Halaman Tambah Prodi</header>
+        <form action="" method="post">
+          <div class="field name">
+            <div class="input-area">
+              <input type="text" name="name" id="name" autocomplete="off" placeholder="Nama">
+            </div>
+          </div>
+          <div class="field program">
+            <div class="input-area">
+              <input type="text" name="jurusan" id="jurusan" autocomplete="off" placeholder="Jurusan">
+            </div>
+          </div>
+          <input type="submit" name="submit" value="Kirim">
+        </form>
+      </div>
 </body>
 </html>
