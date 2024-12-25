b0738785248d7bddf6db50dca7f9018096ff5200
LinggaKaindra <lingga.kaindra@gmail.com>
2024-12-25 15:43:22 +0700

    update ubahprodi

--- admin/programs/ubahProdi.php
+++ admin/programs/ubahProdi.php
@@ -20,23 +20,28 @@ if (isset($_POST["submit"])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <h1>Halaman Ubah Prodi</h1>
-    <form action="" method="post">
-        <input type="hidden" name="id" value="<?= $prodi[0]["id"]; ?>">
-        <label for="nama">nama : </label>
-        <input type="text" name="nama" id="nama" autocomplete="off" value="<?= $prodi[0]["name"]; ?>">
-        <br>
-        <label for="jurusan">jurusan : </label>
-        <input type="text" name="jurusan" id="jurusan" autocomplete="off" value="<?= $prodi[0]["faculty"]; ?>">
-        <br>
-        <button type="submit" name="submit">Kirim</button>
-    </form>
-    
+    <link rel="stylesheet" href="../../Assets/CSS/LoginPetugasSiswaStyle.css">
+    <title>Ubah Data Prodi</title>
 </head>
 <body>
-    
+  <input type="hidden" name="id" value="<?= $prodi[0]["id"]; ?>">
+      <div class="wrapper">
+        <header>Halaman Ubah Prodi</header>
+        <form action="" method="post">
+          <div class="field name">
+            <div class="input-area">
+              <input type="text" name="nama" id="nama" autocomplete="off" value="<?= $prodi[0]["name"]; ?>">
+            </div>
+          </div>
+          <div class="field faculty">
+            <div class="input-area">
+              <input type="text" name="jurusan" id="jurusan" autocomplete="off" value="<?= $prodi[0]["faculty"]; ?>">
+            </div>
+          </div>
+          <input type="submit" name="submit" value="Kirim">
+        </form>
+      </div>
 </body>
 </html>
