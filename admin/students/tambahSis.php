308dcc4424dfe3b8cfaf866d8baacfb0157502da
LinggaKaindra <lingga.kaindra@gmail.com>
2024-12-25 12:43:23 +0700

    update tambahSis.php file

--- admin/students/tambahSis.php
+++ admin/students/tambahSis.php
@@ -21,38 +21,49 @@ if (isset($_POST["submit"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Register</title>
+    <link rel="stylesheet" href="../../Assets/CSS/LoginPetugasSiswaStyle.css">
+    <title>Registrasi Petugas</title>
 </head>
 <body>
-
-    <h1>Halaman Register Siswa</h1>
-
-    <form action="" method="post">
-        <label for="nim">nim : </label>
-        <input type="text" name="nim" id="nim" autocomplete="off">
-        <br>
-        <label for="name">name : </label>
-        <input type="text" name="name" id="name" autocomplete="off">
-        <br>
-        <label for="email">email : </label>
-        <input type="email" name="email" id="email" autocomplete="off">
-        <br>
-        <label for="password">password : </label>
-        <input type="password" name="password" id="password" autocomplete="off">
-        <br>
-        <label for="phone">No Telp. : </label>
-        <input type="text" name="phone" id="phone" autocomplete="off">
-        <br>
-        <label for="program_id">Pilih Kelas : </label>
-        
-        <select name="program_id" id="program_id">
-            <?php foreach ($programs as $row): ?>
-                <option value="<?php echo $row['id']; ?>"><?= $row['faculty'] ?></option>
-            <?php endforeach; ?>
-        </select>
-        <br>
-        <button type="submit" name="submit">Kirim</button>
-    </form>
-    
+      <div class="wrapper">
+        <header>Halaman Register Siswa</header>
+        <form action="" method="post">
+          <div class="field idnumber">
+            <div class="input-area">
+              <input type="text" name="nim"  id="nim" autocomplete="off" placeholder="NIM">
+            </div>
+          </div>
+          <div class="field name">
+            <div class="input-area">
+              <input type="text" name="name"  id="name" autocomplete="off" placeholder="Name">
+            </div>
+          </div>
+          <div class="field email">
+            <div class="input-area">
+              <input type="email" name="email" id="email" autocomplete="off" placeholder="Email">
+            </div>
+          </div>
+          <div class="field password">
+            <div class="input-area">
+              <input type="password" name="password" id="password" autocomplete="off" placeholder="Password">
+            </div>
+          </div>
+          <div class="field phone">
+            <div class="input-area">
+              <input type="text" name="phone" id="phone" autocomplete="off" placeholder="No. Telp">
+            </div>
+          </div>
+          <div class="field program_id">
+            <div class="select-area">
+              <select name="program_id" id="program_id">
+                <?php foreach ($programs as $row): ?>
+                    <option value="<?php echo $row['id']; ?>"><?= $row['faculty'] ?></option>
+                <?php endforeach; ?>
+              </select>
+            </div>
+          </div>
+          <input type="submit" value="Kirim">
+        </form>
+      </div>
 </body>
 </html>
