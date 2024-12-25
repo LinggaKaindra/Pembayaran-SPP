5a8211e05f78275d6d285d9e88e18b41b4e04c41
LinggaKaindra <lingga.kaindra@gmail.com>
2024-12-25 15:30:53 +0700

    update ubahPaymentMethod

--- admin/payment_methods/ubahPaymentMethod.php
+++ admin/payment_methods/ubahPaymentMethod.php
@@ -20,20 +20,23 @@ if (isset($_POST["submit"])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <h1>Halaman Tambah payment_methods</h1>
-    <form action="" method="post">
-        <input type="hidden" name="id" value="<?= $method[0]["id"]; ?>">
-        <label for="name">name : </label>
-        <input type="text" name="name" id="name" autocomplete="off" value="<?= $method[0]["name"]; ?>">
-        <br>
-        <button type="submit" name="submit">Kirim</button>
-    </form>
-    
+    <link rel="stylesheet" href="../../Assets/CSS/LoginPetugasSiswaStyle.css">
+    <title>Ubah Metode Pembayaran</title>
 </head>
 <body>
-    
+  <input type="hidden" name="id" value="<?= $method[0]["id"]; ?>">
+      <div class="wrapper">
+        <header>Ubah Metode Pembayaran</header>
+        <form action="" method="post">
+          <div class="field name">
+            <div class="input-area">
+              <input type="text" name="name" id="name" autocomplete="off" value="<?= $method[0]["name"]; ?>">
+            </div>
+          </div>
+          <input type="submit" name="submit" value="Kirim">
+        </form>
+      </div>
 </body>
 </html>
