73b2b2d79996a44b72dcb0a54f3fc6a4eac512b4
LinggaKaindra <lingga.kaindra@gmail.com>
2025-01-08 15:54:08 +0700

    Update index.php

--- siswa/pembayaran/index.php
+++ siswa/pembayaran/index.php
@@ -43,7 +43,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Data Pembayaran Siswa</title>
     <link rel="stylesheet" href="../../assets/css/StyleSiswa.css">
 </head>
 <body>  
