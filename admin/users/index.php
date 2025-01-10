412ad76942004642b70dae9c89359453fa8c2cc1
LinggaKaindra <lingga.kaindra@gmail.com>
2025-01-10 09:41:26 +0700

    change title

--- admin/users/index.php
+++ admin/users/index.php
@@ -16,7 +16,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../Assets/CSS/tablestyle.css">
-    <title>Document</title>
+    <title>Data Pengguna</title>
 </head>
 <body>
     <center>
