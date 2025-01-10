47d238ddf5b8a233ed263205e17e60e95175b086
LinggaKaindra <lingga.kaindra@gmail.com>
2025-01-10 09:46:35 +0700

    add documentation

--- admin/payments/tambahPemb.php
+++ admin/payments/tambahPemb.php
@@ -29,67 +29,112 @@ $payment_methods = query("SELECT * FROM payment_methods");
 ?>
 
 <!DOCTYPE html>
+<!-- Deklarasi tipe dokumen HTML5 untuk memastikan kompatibilitas dengan standar HTML5 -->
 <html lang="en">
+<!-- Tag pembuka HTML, atribut lang="en" menunjukkan bahwa bahasa dokumen adalah Bahasa Inggris -->
+
 <head>
+    <!-- Elemen <head> berisi metadata dokumen dan elemen-elemen yang tidak langsung ditampilkan di halaman -->
+    
     <meta charset="UTF-8">
+    <!-- Menentukan karakter encoding dokumen sebagai UTF-8 untuk mendukung berbagai karakter -->
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Mengatur agar halaman dapat disesuaikan dengan lebar layar perangkat, penting untuk desain responsif -->
+    
     <title>Document</title>
+    <!-- Judul halaman yang akan ditampilkan di tab browser -->
+    
     <link rel="stylesheet" href="../../Assets/CSS/LoginPetugasSiswaStyle.css">
+    <!-- Menyertakan file CSS eksternal untuk memberikan gaya pada halaman -->
 </head>
+
 <body>
+    <!-- Elemen <body> berisi konten utama halaman yang akan ditampilkan kepada pengguna -->
 
     <div class="wrapper">
+        <!-- Elemen <div> dengan class "wrapper" untuk membungkus seluruh konten -->
+
         <header>Halaman Registrasi Pembayaran</header>
+        <!-- Elemen <header> untuk menampilkan judul bagian "Halaman Registrasi Pembayaran" -->
+
         <form action="" method="post">
+            <!-- Elemen <form> digunakan untuk mengirimkan data input ke server, dengan atribut action="" (URL target) dan method="post" -->
+
             <input type="hidden" name="ukt_id" value="<?= $ukt_id ?>">
+            <!-- Elemen <input> dengan type="hidden" untuk menyembunyikan data "ukt_id" yang akan dikirimkan melalui form -->
+
             <input type="hidden" name="petugas_id" value="<?= $petugas_id ?>">
-            
+            <!-- Elemen <input> dengan type="hidden" untuk menyembunyikan data "petugas_id" yang akan dikirimkan melalui form -->
+
             <div class="field name">
+                <!-- Elemen <div> dengan class "field name" untuk membungkus input teks -->
+
                 <div class="input-area">
+                    <!-- Elemen <div> dengan class "input-area" untuk mengatur area input -->
                     <input type="text" name="nim" id="nim" autocomplete="off" value="<?= $nim; ?>">
+                    <!-- Input teks untuk "nim" mahasiswa, dengan nilai diambil dari variabel PHP -->
                 </div>
             </div>
+
             <div class="field name">
                 <div class="input-area">
-                <input type="text" name="name" id="name" autocomplete="off" value="<?= $ukt[0]['name']; ?>" disabled>
+                    <input type="text" name="name" id="name" autocomplete="off" value="<?= $ukt[0]['name']; ?>" disabled>
+                    <!-- Input teks untuk "name" mahasiswa, nilai diambil dari variabel PHP, dan atribut disabled membuatnya tidak dapat diedit -->
                 </div>
             </div>
+
             <div class="field name">
                 <div class="input-area">
-                <input type="text" name="facultas" id="facultas" autocomplete="off" value="<?= $student[0]['faculty']; ?>" disabled>
+                    <input type="text" name="facultas" id="facultas" autocomplete="off" value="<?= $student[0]['faculty']; ?>" disabled>
+                    <!-- Input teks untuk "faculty", nilai diambil dari variabel PHP, dan tidak dapat diedit -->
                 </div>
             </div>
+
             <div class="field name">
                 <div class="input-area">
-                <input type="text" name="tahun_ajaran" id="tahun" autocomplete="off" value="<?= $ukt[0]['year']; ?>" disabled>
+                    <input type="text" name="tahun_ajaran" id="tahun" autocomplete="off" value="<?= $ukt[0]['year']; ?>" disabled>
+                    <!-- Input teks untuk "tahun ajaran", nilai diambil dari variabel PHP, dan tidak dapat diedit -->
                 </div>
             </div>
+
             <div class="field name">
                 <div class="input-area">
-                <input type="text" name="semester" id="semester" autocomplete="off" value="<?= $ukt[0]['semester']; ?>" disabled>
+                    <input type="text" name="semester" id="semester" autocomplete="off" value="<?= $ukt[0]['semester']; ?>" disabled>
+                    <!-- Input teks untuk "semester", nilai diambil dari variabel PHP, dan tidak dapat diedit -->
                 </div>
             </div>
+
             <div class="field name">
                 <div class="input-area">
-                <input type="text" name="amount" id="amount" autocomplete="off" value="Rp. <?= $ukt[0]['amount']; ?>">
+                    <input type="text" name="amount" id="amount" autocomplete="off" value="Rp. <?= $ukt[0]['amount']; ?>">
+                    <!-- Input teks untuk "amount" pembayaran, nilai diambil dari variabel PHP -->
                 </div>
             </div>
-    
+
             <div class="field">
+                <!-- Elemen <div> untuk membungkus elemen <select> -->
+                
                 <div class="select-area">
-                  <select name="payment_method_id" id="payment_method_id">
-                    <?php foreach ($payment_methods as $row): ?>
+                    <!-- Elemen <div> dengan class "select-area" untuk mengatur area dropdown -->
+
+                    <select name="payment_method_id" id="payment_method_id">
+                        <!-- Elemen <select> untuk memilih metode pembayaran -->
+
+                        <?php foreach ($payment_methods as $row): ?>
+                        <!-- Looping PHP untuk menampilkan setiap metode pembayaran dalam dropdown -->
+
                         <option value="<?php echo $row['id']; ?>"><?= $row['name'] ?></option>
-                    <?php endforeach; ?>
-                  </select>
+                        <!-- Elemen <option> untuk setiap metode pembayaran, dengan nilai diambil dari variabel PHP -->
+                        
+                        <?php endforeach; ?>
+                    </select>
                 </div>
-              </div>
-    
+            </div>
+
             <input type="submit" name="submit"></input>
+            <!-- Tombol submit untuk mengirimkan form -->
         </form>
     </div>
-    
-<body>
-    
 </body>
 </html>
